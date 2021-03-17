6ac890de73d8d0a911c27246f791dbf77641cda8
Ilya Baskakov <baskakov.11111@mail.ru>
2021-03-17 18:56:52 +0300

    optimizing with imagemin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import _ from 'lodash'
+import './styles/style.css'
+// import Logo from './assets/radiation.png'
 
-function component() {
-  const element = document.createElement('div')
-
-  element.innerHTML = _.join(['Hello', 'from', 'webpack'], ' ')
-
-  return element
-}
-
-document.body.appendChild(component())
+console.log('hello')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,70 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['mozjpeg', { quality: 50 }],
+          ['optipng', { optimizationLevel: 7 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: ImageMinimizerPlugin.loader,
+      //       options: {
+      //         severityError: 'warning', // Ignore errors on corrupted images
+      //         minimizerOptions: {
+      //           plugins: ['gifsicle'],
+      //         },
+      //       },
+      //     },
+      //   ],
+      // }
+    ],
   },
 }
